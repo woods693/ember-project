e2224a1a0e4ecbde1c75ba1b6bb48136f8ea4f62
woodyc <woodyc@sfu.ca>
2017-06-25 18:25:28 -0700

    pushing homepage

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('tasks', function() {
     this.route('new');
   });
+  this.route('homepage');
 });
 
 export default Router;
---
+++ app/routes/homepage.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model(){
+        return ['Chris Lee', 'Jason Chang', 'Woody Chang', 'Chang Wook Ahn', 'Brandon Nguyen', 'Alin Paunescu'];
+    },
+    actions:{
+        statementLink(){
+            window.location.replace("https://guides.emberjs.com/v2.13.0/")
+        },
+        
+        starting(){
+            window.location.replace("https://emberjs.com/");
+        }
+    }
+});
