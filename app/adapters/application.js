5093d4d257b065568c9f23540f904d595f4208eb
Alin Paunescu <apaunesc@sfu.ca>
2017-06-26 21:58:13 -0700

    Authentication Feature

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,11 @@
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
+	namespace: 'api',
+	session: Ember.inject.service(),
+	headers: Ember.computed('session.token', function(){
+		return{
+			'Authorization': `Bearer ${this.get('session.token')}`
+		};
+	})
 });
